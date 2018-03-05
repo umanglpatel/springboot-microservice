71596e12e4b93515f26b5e9da4dd73766a078545
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-03-05 17:21:46 +0530

    Add api-gateway project. Use Netflix zuul for routing requests.

---
+++ api-gateway/src/main/java/com/demo/api/ApiGatewayApplication.java
@@ -0,0 +1,37 @@
+package com.demo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.demo.api.filters.*;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+}
