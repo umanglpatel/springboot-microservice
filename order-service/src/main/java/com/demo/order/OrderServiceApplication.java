57c1cca6f1a90f0d56bb3530336c0c6bf8581459
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-03-06 17:47:39 +0530

    User spring EnableDiscoveryClient annotation for clients to avoid specific eureka annotation

--- order-service/src/main/java/com/demo/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/demo/order/OrderServiceApplication.java
@@ -2,10 +2,11 @@ package com.demo.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+// @EnableEurekaClient
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/com/demo/user/UserServiceApplication.java
+++ user-service/src/main/java/com/demo/user/UserServiceApplication.java
@@ -2,10 +2,11 @@ package com.demo.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+// @EnableEurekaClient // this is eureka specific
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
