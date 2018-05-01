29a86719a098ef671fdbf399e6132317b9df743b
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-05-01 15:22:22 +0530

    Add logback sample logging. Use slf4j as writing logs. slf4j is facade and logback implements it

--- order-service/src/main/java/com/demo/order/controller/OrderController.java
+++ order-service/src/main/java/com/demo/order/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.demo.order.controller;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +21,12 @@ public class OrderController {
 	@Autowired
 	private OrderService orderService;
 
+	private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+	// https://springframework.guru/using-logback-spring-boot/
+	// SLF4J is a façade for commonly used logging frameworks,
+	// such as Java Util Logging, Log4J 2, and Logback.
+	// By writing against SLF4J, our code remains decoupled
+
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Order> addOrder(@RequestBody Order order) {
 		order = orderService.addOrder(order);
@@ -29,6 +37,10 @@ public class OrderController {
 	public ResponseEntity<String> test() throws UnknownHostException {
 		InetAddress ip = InetAddress.getLocalHost();
 		String result = "Request served from : " + ip.getHostAddress() + " and host : " + ip.getHostName();
+		logger.debug("Logback : This is a debug message");
+		logger.info("Logback : This is an info message");
+		logger.warn("Logback : This is a warn message");
+		logger.error("Logback : This is an error message");
 		return new ResponseEntity<String>(result, HttpStatus.OK);
 	}
 }
