a0c3e046522adcbf2cdccb5d69798b3c1d35f143
Umang Patel <umang.patel@AMD.IBASET.COM>
2019-04-01 11:40:22 +0530

    Remove cros origin from order controller as it conflicts with cors config in api gateway project. It should only have one cors config.

--- order-service/src/main/java/com/demo/order/controller/OrderController.java
+++ order-service/src/main/java/com/demo/order/controller/OrderController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import com.demo.domain.data.Order;
 import com.demo.order.service.OrderService;
 
-@CrossOrigin
+//@CrossOrigin
 @RestController
 @RequestMapping("/v1.0/orders")
 public class OrderController {
